bb808aa9a71ca0a44a7f2daa90798db104379785
121855 <whr_61@hotmail.com>
2022-07-24 01:04:27 +0800

    Initial commit

---
+++ provider-payment8001/src/main/java/com/whr/springcloud/PaymentMain8001.java
@@ -0,0 +1,16 @@
+package com.whr.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Reed
+ * @Description
+ * @date 2022/7/23 23:36
+ **/
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class,args);
+    }
+}
