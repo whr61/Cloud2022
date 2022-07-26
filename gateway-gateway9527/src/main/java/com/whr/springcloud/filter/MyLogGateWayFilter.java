48838c068f8315b447f999cda46640819fabfba8
121855 <whr_61@hotmail.com>
2022-07-26 21:24:20 +0800

    网关增加鉴权认证

---
+++ gateway-gateway9527/src/main/java/com/whr/springcloud/filter/MyLogGateWayFilter.java
@@ -0,0 +1,34 @@
+package com.whr.springcloud.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+import java.util.Date;
+
+/**
+ * @author Reed
+ * @Description
+ * @date 2022/7/26 21:07
+ **/
+@Component
+public class MyLogGateWayFilter implements GlobalFilter, Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        System.out.println("**************come in MyLogGateWayFilter:  " + new Date());
+        String uname = exchange.getRequest().getQueryParams().getFirst("uname");
+        if (uname == null) {
+            System.out.println("********用户名为空，非法用户。");
+            exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+            return exchange.getResponse().setComplete();
+        }
+        return chain.filter(exchange);
+    }
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
