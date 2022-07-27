b9765cdc653e6cfd2eae4a9a80670235332905b7
121855 <whr_61@hotmail.com>
2022-07-27 21:24:56 +0800

    Nacos注册中心

---
+++ alibaba-provider-payment9001/src/main/java/com/whr/springcloud/alibaba/PaymentMain9001.java
@@ -0,0 +1,18 @@
+package com.whr.springcloud.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author Reed
+ * @Description
+ * @date 2022/7/27 20:54
+ **/
+@EnableDiscoveryClient
+@SpringBootApplication
+public class PaymentMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9001.class,args);
+    }
+}
