68ec8c018883f712dc32f6f9ec609e5447e85d6d
121855 <whr_61@hotmail.com>
2022-07-28 21:45:42 +0800

    Nacos Config配置

---
+++ alibaba-config-nacos-client3377/src/main/java/com/whr/springcloud/alibaba/NacosConfigClientMain3377.java
@@ -0,0 +1,18 @@
+package com.whr.springcloud.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author Reed
+ * @Description
+ * @date 2022/7/28 20:42
+ **/
+@EnableDiscoveryClient
+@SpringBootApplication
+public class NacosConfigClientMain3377 {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientMain3377.class,args);
+    }
+}
