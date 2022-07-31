0e8d7f0711e54eb88a201add24430dc6da29acd6
121855 <whr_61@hotmail.com>
2022-07-31 10:56:31 +0800

    11

--- alibaba-sentinel-service8401/src/main/java/com/whr/springcloud/alibaba/controller/FlowLimitController.java
+++ alibaba-sentinel-service8401/src/main/java/com/whr/springcloud/alibaba/controller/FlowLimitController.java
@@ -15,6 +15,7 @@ public class FlowLimitController {
     @GetMapping("/testA")
     public String testA(){
         try{
+            System.out.println("1111111111");
             TimeUnit.MILLISECONDS.sleep(850);
         }catch(InterruptedException e){
             e.printStackTrace();
