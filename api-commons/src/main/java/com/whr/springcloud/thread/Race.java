4033ce1d99405b0940619e87cb9f3a06153d37ea
121855 <whr_61@hotmail.com>
2022-08-02 21:16:33 +0800

    多线程龟兔赛跑与静态代理模式

---
+++ api-commons/src/main/java/com/whr/springcloud/thread/Race.java
@@ -0,0 +1,46 @@
+package com.whr.springcloud.thread;
+
+/**
+ * @author Reed
+ * @Description
+ * @date 2022/8/2 19:55
+ **/
+
+public class Race implements Runnable{
+    private static String winner;
+    @Override
+    public void run(){
+        for (int i = 0; i <= 100; i++) {
+            if(Thread.currentThread().getName().equals("兔子") && i%10==0){
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            boolean flag = gameOver(i);
+            if(flag){
+                break;
+            }
+            System.out.println(Thread.currentThread().getName()+"-->跑了"+i+"步");
+        }
+    }
+    private boolean gameOver(int steps){
+        if(winner!=null){
+            return true;
+        }{
+            if(steps>=100){
+                winner = Thread.currentThread().getName();
+                System.out.println("winner is"+winner);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Race race = new Race();
+        new Thread(race,"兔子").start();
+        new Thread(race,"乌龟").start();
+    }
+}
