228292daf24cfde3a15aa5dadcc77975d5605ee9
121855 <whr_61@hotmail.com>
2022-08-23 21:00:06 +0800

    线程停止

---
+++ api-commons/src/main/java/com/whr/springcloud/thread/TestStop.java
@@ -0,0 +1,33 @@
+package com.whr.springcloud.thread;
+
+/**
+ * @author Reed
+ * @Description
+ * @date 2022/8/23 20:49
+ **/
+
+public class TestStop implements Runnable{
+    private boolean flag = true;
+    @Override
+    public void run(){
+        int i = 0;
+        while (flag){
+            System.out.println("run....Thread"+i++);
+        }
+    }
+    public void stop(){
+        this.flag = false;
+    }
+
+    public static void main(String[] args) {
+        TestStop testStop =  new TestStop();
+        new Thread(testStop).start();
+        for (int i = 0; i < 1000; i++) {
+            System.out.println("main"+i);
+            if(i==900){
+                testStop.stop();
+                System.out.println("线程该停止了");
+            }
+        }
+    }
+}
