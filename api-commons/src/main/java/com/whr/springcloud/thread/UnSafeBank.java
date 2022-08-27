56b3239b31efb5f97ca75abd2d4da14eab92cd83
121855 <whr_61@hotmail.com>
2022-08-27 22:39:43 +0800

    线程更新

---
+++ api-commons/src/main/java/com/whr/springcloud/thread/UnSafeBank.java
@@ -0,0 +1,59 @@
+package com.whr.springcloud.thread;
+
+/**
+ * @author Reed
+ * @Description
+ * @date 2022/8/27 22:07
+ **/
+
+public class UnSafeBank {
+    public static void main(String[] args) {
+        Account account = new Account(100,"结婚基金");
+        Drawing you = new Drawing(account,50,"你");
+        Drawing girlFriend =  new Drawing(account,100,"女朋友");
+        you.start();
+        girlFriend.start();
+    }
+}
+//账户
+class Account{
+    int money;//余额
+    String name;//卡名
+    public Account(int money, String name) {
+        this.money = money;
+        this.name = name;
+    }
+}
+//银行:模拟取款
+class Drawing extends Thread{
+    //账户
+    Account account;
+    //取了多少钱
+    int drawingMoney;
+    //手上的钱
+    int nowMoney;
+    public Drawing(Account account,int drawingMoney,String name){
+        super(name);
+        this.account = account;
+        this.drawingMoney = drawingMoney;
+    }
+    //取钱
+    @Override
+    public void run(){
+        //判断有没有钱
+        if(account.money-drawingMoney<0){
+            System.out.println(Thread.currentThread().getName()+"钱不够，取不了");
+            return;
+        }
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        account.money = account.money-drawingMoney;
+        nowMoney = nowMoney+drawingMoney;
+        System.out.println(account.name+"余额为:"+account.money);
+        //Thread.currentThread().getName()=this.getName()
+        System.out.println(this.getName()+"手里的钱:"+nowMoney);
+    }
+}
---
+++ api-commons/src/main/java/com/whr/springcloud/thread/UnSafeBuyTicket.java
@@ -0,0 +1,41 @@
+package com.whr.springcloud.thread;
+
+/**
+ * @author Reed
+ * @Description
+ * @date 2022/8/27 21:42
+ **/
+
+public class UnSafeBuyTicket {
+    public static void main(String[] args) {
+        BuyTicket buyTicket = new BuyTicket();
+        new Thread(buyTicket,"苦逼的我").start();
+        new Thread(buyTicket,"牛逼的你们").start();
+        new Thread(buyTicket,"可恨的黄牛党").start();
+    }
+}
+class BuyTicket implements Runnable {
+    //票
+    private int ticketNums = 10;
+    //外部停止方式
+    boolean flag = true;
+    @Override
+    public void run() {
+        try {
+            while (flag){
+                buy();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    private void buy() throws InterruptedException {
+        if(ticketNums<=0){
+            flag = false;
+            return;
+        }
+        Thread.sleep(100);
+        //买票
+        System.out.println(Thread.currentThread().getName()+"拿到"+ticketNums--);
+    }
+}
