f6b5b98d09e34d4f1e2de9382bafb3f23c670d75
121855 <whr_61@hotmail.com>
2022-10-02 20:27:52 +0800

    工厂模式

---
+++ api-commons/src/main/java/com/whr/factory/simplefactory/pizzastore/pizza/GreekPizza.java
@@ -0,0 +1,10 @@
+package com.whr.factory.simplefactory.pizzastore.pizza;
+
+public class GreekPizza extends Pizza {
+
+    @Override
+    public void prepare() {
+        // TODO Auto-generated method stub
+        System.out.println(" 给制作希腊披萨 准备原材料 ");
+    }
+}
