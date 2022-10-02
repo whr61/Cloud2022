9566b1e3ccd326c84039c3867ff9e994507468c5
121855 <whr_61@hotmail.com>
2022-10-02 22:02:38 +0800

    工厂方法模式

--- api-commons/src/main/java/com/whr/factory/factorymethod/pizzastore/order/BJOrderPizza.java
+++ api-commons/src/main/java/com/whr/factory/factorymethod/pizzastore/order/BJOrderPizza.java
@@ -4,13 +4,9 @@ import com.whr.factory.factorymethod.pizzastore.pizza.BJCheesePizza;
 import com.whr.factory.factorymethod.pizzastore.pizza.BJPepperPizza;
 import com.whr.factory.factorymethod.pizzastore.pizza.Pizza;
 
-
 public class BJOrderPizza extends OrderPizza {
-
-
     @Override
     Pizza createPizza(String orderType) {
-
         Pizza pizza = null;
         if(orderType.equals("cheese")) {
             pizza = new BJCheesePizza();
@@ -20,5 +16,4 @@ public class BJOrderPizza extends OrderPizza {
         // TODO Auto-generated method stub
         return pizza;
     }
-
 }
