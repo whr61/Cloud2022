e6e5bc0909f859d0525ca8cc47736fd9f91ec2f6
121855 <whr_61@hotmail.com>
2022-10-02 21:24:01 +0800

    工厂模式

--- api-commons/src/main/java/com/whr/factory/simplefactory/pizzastore/order/PizzaStore.java
+++ api-commons/src/main/java/com/whr/factory/simplefactory/pizzastore/order/PizzaStore.java
@@ -5,13 +5,6 @@ public class PizzaStore {
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        //new OrderPizza();
-
-        //使用简单工厂模式
-        //new OrderPizza(new SimpleFactory());
-        //System.out.println("~~退出程序~~");
-
-        new OrderPizza2();
+        new OrderPizza();
     }
-
 }
