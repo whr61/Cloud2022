9b8e0370d82237dd2a7846cdb007a4d46ac36f0d
121855 <whr_61@hotmail.com>
2022-10-03 09:06:27 +0800

    抽象工厂模式

---
+++ api-commons/src/main/java/com/whr/factory/absfactory/pizzastore/order/BJFactory.java
@@ -0,0 +1,21 @@
+package com.whr.factory.absfactory.pizzastore.order;
+
+import com.whr.factory.absfactory.pizzastore.pizza.BJCheesePizza;
+import com.whr.factory.absfactory.pizzastore.pizza.BJPepperPizza;
+import com.whr.factory.absfactory.pizzastore.pizza.Pizza;
+
+//这是工厂子类
+public class BJFactory implements AbsFactory {
+
+    @Override
+    public Pizza createPizza(String orderType) {
+        System.out.println("~使用的是抽象工厂模式~");
+        Pizza pizza = null;
+        if (orderType.equals("cheese")) {
+            pizza = new BJCheesePizza();
+        } else if (orderType.equals("pepper")) {
+            pizza = new BJPepperPizza();
+        }
+        return pizza;
+    }
+}
