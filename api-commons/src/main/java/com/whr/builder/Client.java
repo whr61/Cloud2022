2e4a72ffc9a468e2492461039eda0191605e889d
121855 <whr_61@hotmail.com>
2022-10-04 21:28:07 +0800

    建造者模式

---
+++ api-commons/src/main/java/com/whr/builder/Client.java
@@ -0,0 +1,10 @@
+package com.whr.builder;
+
+public class Client {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        CommonHouse commonHouse = new CommonHouse();
+        commonHouse.build();
+    }
+}
