016f64ca4b030fdc62c844208af865dbd5eaa076
121855 <whr_61@hotmail.com>
2022-10-05 10:38:55 +0800

    适配器模式

---
+++ api-commons/src/main/java/com/whr/adapter/interfaceadapter/AbsAdapter.java
@@ -0,0 +1,16 @@
+package com.whr.adapter.interfaceadapter;
+
+public abstract class AbsAdapter implements Interface4 {
+
+	public void m1() {
+	}
+
+	public void m2() {
+	}
+
+	public void m3() {
+	}
+
+	public void m4() {
+	}
+}
